f834fc1b1d178bf641f1da7aab1d8402ce01e937
KoichiF <fumikatsu.koichi@gmail.com>
2026-01-28 16:44:46 -0500

    Version 3.0.0.0 AZCKeeper - Commit 14 - Revision Completa de lo que se lelva hasta ahora, se encuentran parches mal aplicados por lo que es encesario el corregirlos

--- obj/Debug/net8.0-windows/AZCKeeper_Client.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/AZCKeeper_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AZCKeeper_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a88aa852fae5b8fdcf3c976d8c21359c65054645")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31f0f0d35a1dfc7d5bc487bf5311b2815f33de43")]
 [assembly: System.Reflection.AssemblyProductAttribute("AZCKeeper_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AZCKeeper_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
